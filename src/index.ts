5c91d371ca411bb229c965436d4592fe3ff11f2e
nanqic <nanqic@163.com>
2024-03-20 08:43:40 +0800

    feat: request stat

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import { corsHeaders, proxySearch, proxySearchDetail } from "./requests";
 
 export interface Env {
 	SEARCH_CACHE: KVNamespace
+	SEARCH_STAT: KVNamespace
 }
+
+export const formattedDate = () => (new Date()).toLocaleDateString('zh-CN')
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const setCache = async (key: string, data: string) => env.SEARCH_CACHE.put(key, data);
 		const getCache = async (key: string) => env.SEARCH_CACHE.get(key);
+		const getStat = async (key: string) => env.SEARCH_STAT.get(key);
+		const setStat = async (key: string, data: string) => env.SEARCH_STAT.put(key, data);
 		const getCacheList = async () => env.SEARCH_CACHE.list();
 		const url = new URL(request.url);
 		const searchParams = new URLSearchParams(url.search)
@@ -16,7 +21,9 @@ export default {
 		const jsonParam = decodeURI(searchParams.get('json') || '')
 
 		// console.log(`Hello ${navigator.userAgent} at path ${url.pathname}!`, keywordsParam);
+		const statIncrease = async (key: string) => await setStat(key, parseInt((await getStat(key)) || '0') + 1 + '')
 
+		await statIncrease('request_since_0320')
 		if (url.pathname === "/api" && keywordsParam != ''
 			&& request.method == 'POST') {
 			if (pageParam == '1') {
@@ -31,11 +38,17 @@ export default {
 				})
 			}
 
+			await statIncrease(formattedDate())
+			await statIncrease('cache_since_0320')
 			return await proxySearch(request, setCache, keywordsParam, pageParam)
 		} else if (url.pathname === "/api/q" && request.method == 'GET') {
+			await statIncrease('detail_since_0320')
 
+			return await proxySearchDetail(jsonParam)
 		}
 
-		return await proxySearchDetail(jsonParam)
+		await statIncrease('illegal_since_0320')
+
+		return new Response(null)
 	},
 };
